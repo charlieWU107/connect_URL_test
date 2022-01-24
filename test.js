d917cf0e33905b654b6a4d35adceabd4d4f57b6d
charlieWU107 <charlie107@linfair.com.tw>
2022-01-24 16:05:55 +0800

    charlieWU107

--- test.js
+++ test.js
@@ -1,57 +1,23 @@
-videojs.registerPlugin('URL_test', function() {
-    var myPlayer = this,
-        jumpAmount = 5,
-        controlBar,
-        insertBeforeNode,
-        newElementBB = document.createElement('div'),
-        newElementFB = document.createElement('div'),
-        newImageBB = document.createElement('img'),
-        newImageFB = document.createElement('img');
-  
-    // +++ Assign IDs for later element manipulation +++
-    newElementBB.id = 'backButton';
-    newElementFB.id = 'forwardButton';
-  
-    // +++ Assign properties to elements and assign to parents +++
-    newImageBB.setAttribute('src', '//learning-services-media.brightcove.com/doc-assets/player-development/samples/back-forward-buttons/back-button.png');
-    newElementBB.appendChild(newImageBB);
-    newImageFB.setAttribute('src', '//learning-services-media.brightcove.com/doc-assets/player-development/samples/back-forward-buttons/forward-button.png');
-    newElementFB.appendChild(newImageFB);
-  
-    // +++ Get controlbar and insert elements +++
-    controlBar = myPlayer.$('.vjs-control-bar');
-    // Get the element to insert buttons in front of in conrolbar
-    insertBeforeNode = myPlayer.$('.vjs-volume-panel');
-  
-    // Insert the button div in proper location
-    controlBar.insertBefore(newElementBB, insertBeforeNode);
-    controlBar.insertBefore(newElementFB, insertBeforeNode);
-  
-    // +++ Add event handlers to jump back or forward +++
-    // Back button logic, don't jump to negative times
-    newElementBB.addEventListener('click', function () {
-      var newTime,
-          rewindAmt = jumpAmount,
-          videoTime = myPlayer.currentTime();
-      if (videoTime >= rewindAmt) {
-        newTime = videoTime - rewindAmt;
-      } else {
-        newTime = 0;
-      }
-      myPlayer.currentTime(newTime);
-    });
-  
-    // Forward button logic, don't jump past the duration
-    newElementFB.addEventListener('click', function () {
-      var newTime,
-          forwardAmt = jumpAmount,
-          videoTime = myPlayer.currentTime(),
-          videoDuration = myPlayer.duration();
-      if (videoTime + forwardAmt <= videoDuration) {
-        newTime = videoTime + forwardAmt;
-      } else {
-        newTime = videoDuration;
-      }
-      myPlayer.currentTime(newTime);
-    });
-  });
+var myPlayer;
+
+// Get the PLAYER and be sure the PLAYER is ready to interact with
+videojs.getPlayer("myPlayerID").ready(function() {
+  //  Get a reference to the PLAYER
+  myPlayer = this;
+  // +++ Add the class to the player +++
+  myPlayer.addClass("myClass");
+});
+
+// +++ Function called by button that toggles class +++
+function toggleControlbar() {
+  myPlayer.toggleClass("myClass");
+}
+<pre>
+======
+
+<input 
+type="button"value="註冊" 
+onclick="javascript:window.location.href='http://www.youtube.com'"/>
+
+======
+</pre> 
